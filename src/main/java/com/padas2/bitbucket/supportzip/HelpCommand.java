e11b3a7c3cbb7daaa341e93d3246c69ca71583f1
padas2 <Supreeth.Padavala@in.pega.com>
2019-05-15 00:34:12 +0530

    Adding sample usage syntax to CLI

--- src/main/java/com/padas2/bitbucket/supportzip/HelpCommand.java
+++ src/main/java/com/padas2/bitbucket/supportzip/HelpCommand.java
@@ -16,8 +16,19 @@ public class HelpCommand implements ExecutableCommand {
         formatter.printHelp("BitbucketSupportZipCLI", options);
     }
 
+    protected void printSampleUsage() {
+        System.out.println("\nSample usage :");
+        System.out.println("java -jar bitbucket-suuportzip-generator-cli-0.0.2-SNAPSHOT.jar " +
+                           "-gitServerUrl http://localhost:7990 " +
+                           "-adminUser <admin-user> " +
+                           "-adminPwd <admin-password> " +
+                           "-showLogsDirInFileExplorer " +
+                           "-flattenUnzipDir");
+    }
+
     @Override
     public void run() {
         printHelpUsing(options);
+        printSampleUsage();
     }
 }
